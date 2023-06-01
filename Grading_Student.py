810443dd137f8690df22764d04a976ab779a3a86
dagota12 <84585431+dagota12@users.noreply.github.com>
2023-06-01 04:17:02 -0500

    Create Grading_Student.py

---
+++ Grading_Student.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+def round(n):
+    if n<38:
+        return n
+    rem = n%5
+    if rem <3:
+        return n
+    else: return n+(5-rem)
+def gradingStudents(grades):
+    return list(map(round,grades))
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
